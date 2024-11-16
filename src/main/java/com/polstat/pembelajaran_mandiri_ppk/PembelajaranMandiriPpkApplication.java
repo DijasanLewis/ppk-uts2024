b8145d5e45953c051d2f6b0882a1348ae7a14eb4
DijasanLewis <yedijalewisuryadi@gmail.com>
2024-11-16 18:20:17 +0700

    Inisialisasi projek spring dan dependensi

---
+++ src/main/java/com/polstat/pembelajaran_mandiri_ppk/PembelajaranMandiriPpkApplication.java
@@ -0,0 +1,13 @@
+package com.polstat.pembelajaran_mandiri_ppk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PembelajaranMandiriPpkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PembelajaranMandiriPpkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/polstat/pembelajaran_mandiri_ppk/PembelajaranMandiriPpkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.polstat.pembelajaran_mandiri_ppk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PembelajaranMandiriPpkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
